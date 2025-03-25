bf21e5229e0d6674f9298cd879408a30daaf947c
iti-rina <itigecheva.irina@gmail.com>
2025-03-25 21:32:54 +0700

    Manage offers mocks using props

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -8,16 +8,18 @@ import { Offer } from '../../pages/Offer/Offer';
 import { EmptyPage } from '../../pages/EmptyPage/EmptyPage';
 import { PrivateRoute } from '../PrivateRoute/PrivateRoute';
 import { AuthStatus } from '../../authStatus';
+import type { OfferType } from '../../mocks/offers';
 
 type AppProps = {
   offerCount: number;
+  offers: OfferType[];
 }
 
-export const App: FC<AppProps> = ({offerCount}) => (
+export const App: FC<AppProps> = ({offerCount, offers}) => (
   <Router>
     <Routes>
-      <Route path={AppRoutes.Main} element={<Main offerCount={offerCount}/>} />
-      <Route path={AppRoutes.Favorites} element={<PrivateRoute authStatus={AuthStatus.NoAuth}><Favorites /></PrivateRoute>} />
+      <Route path={AppRoutes.Main} element={<Main offerCount={offerCount} offers={offers} authStatus={AuthStatus.NoAuth}/>} />
+      <Route path={AppRoutes.Favorites} element={<PrivateRoute authStatus={AuthStatus.NoAuth}><Favorites offers={offers.filter((offer) => offer.isFavorite)}/></PrivateRoute>} />
       <Route path={AppRoutes.Login} element={<Login />} />
       <Route path={AppRoutes.Offer} element={<Offer />} />
       <Route path='*' element={<EmptyPage />} />
