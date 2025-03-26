963165254fc6181c4f70dd2529b863b1f42a1adc
iti-rina <itigecheva.irina@gmail.com>
2025-03-26 14:33:32 +0700

    fix: remove unused variable

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,3 @@
 export function capitalize(text: string = '') {
-  const result = text ? text[0].toUpperCase() + text.substring(1) : '';
-
-  return result;
+  return text ? text[0].toUpperCase() + text.substring(1) : '';
 }
